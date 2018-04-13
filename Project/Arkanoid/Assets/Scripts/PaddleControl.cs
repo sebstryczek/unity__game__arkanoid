feb86dbbebdc05673c8b360f996eac6c6b4b110c
Sebastian <sebastian@stryczek.pl>
2018-04-13 15:30:19 +0200

    Create movable paddle

---
+++ Project/Arkanoid/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PaddleControl : MonoBehaviour
+{
+    private Transform selfTransform;
+    private float extremePosition;
+
+    private void Awake()
+    {
+        this.selfTransform = this.transform;
+        this.extremePosition = this.GetExtremePosition();
+    }
+
+    private void Update()
+    {
+        Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float posX = Mathf.Clamp(worldMousePosition.x, -this.extremePosition, this.extremePosition);
+        this.transform.position = new Vector2(posX, this.transform.position.y);
+    }
+
+    private float GetExtremePosition()
+    {
+        float screenRatio = (float)Screen.width / Screen.height;
+        float viewportHeight = Camera.main.orthographicSize * 2;
+        float viewportWidth = viewportHeight * screenRatio;
+        float halfViewportWidth = viewportWidth / 2;
+        float halfPaddleWidth = this.selfTransform.localScale.x / 2;
+        return halfViewportWidth - halfPaddleWidth;
+    }
+}
