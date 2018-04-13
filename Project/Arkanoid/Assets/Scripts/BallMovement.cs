1328874023685684f543399c47affa6319b88765
Sebastian <sebastian@stryczek.pl>
2018-04-13 16:49:24 +0200

    Add ball and ball movement

---
+++ Project/Arkanoid/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    private void Start()
+    {
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = Vector2.up * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
